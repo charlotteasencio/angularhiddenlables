90fe6afd30af225c14efbdae172bf66fdcc5a786
charlotteasencio <charlotte.e.asencio@gmail.com>
2018-10-24 00:41:20 -0400

    html and css done, focus and blur done but methods aren't correct

---
+++ src/app/signup-form/signup-form.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-signup-form',
+  templateUrl: './signup-form.component.html',
+  styleUrls: ['./signup-form.component.css']
+})
+export class SignupFormComponent  {
+  
+  constructor() { 
+    
+
+  }
+
+  myMethod() { 
+    console.log("focused")
+  }
+
+  myOtherMethod() {
+    console.log("unfocused")
+  }
+
+}
